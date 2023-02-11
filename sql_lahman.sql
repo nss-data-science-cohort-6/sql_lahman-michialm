b00693e7f506b760f030d097f3f6f571710d7053
MichialM <michial.miller@gmail.com>
2023-02-11 14:55:28 -0500

    through question 9

--- sql_lahman.sql
+++ sql_lahman.sql
@@ -34,29 +34,43 @@
 --3. Find the average number of strikeouts per game by decade since 1920. 
 --   Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
 
+--Michael's Code:
+-- WITH decade_cte AS (SELECT generate_series(1920, 2020, 10) AS beginning_of_decade)
+-- SELECT ROUND(SUM(hr) * 1.0 / (SUM(g) / 2), 2) AS hr_per_game,	ROUND(SUM(so) * 1.0 / (SUM(g) / 2), 2) AS so_per_game,
+-- 	beginning_of_decade::text || 's' AS decade
+-- FROM teams
+-- INNER JOIN decade_cte
+-- ON yearid BETWEEN beginning_of_decade AND beginning_of_decade + 9
+-- WHERE yearid >= 1920
+-- GROUP BY decade
+-- ORDER BY decade;
+
+
+-- My Code:
 -- WITH decades AS (
 -- 	SELECT * FROM generate_series(1870, 2021, 10)AS gen)
 -- SELECT gen AS Decade, ROUND(AVG((so / g)), 2) AS avg_strikeouts_per_game
 -- FROM pitching p
 -- INNER JOIN decades
 -- ON p.yearid BETWEEN gen AND gen+9
--- GROUP BY Decade;
+-- GROUP BY Decade
+-- ORDER BY Decade;
 
 --4. Find the player who had the most success stealing bases in 2016, where success is measured as the percentage of stolen base attempts which are successful. 
 --(A stolen base attempt results either in a stolen base or being caught stealing.) 
 --Consider only players who attempted at least 20 stolen bases. 
 --Report the players' names, number of stolen bases, number of attempts, and stolen base percentage.
 
--- SELECT success.nameFirst, success.nameLast, CAST(CAST(Success.stolen_bases AS DECIMAL(5, 2)) / success.total_attempts * 100 AS DECIMAL(5, 2)) AS success_stealing 
--- FROM(SELECT nameFirst, nameLast, SUM(sb) AS stolen_bases, SUM(sb + cs) AS total_attempts
--- 	FROM people
--- 	INNER JOIN batting AS B
--- 	USING(playerid)
--- 	WHERE sb >= 20 
--- 		AND yearid = 2016
--- 	GROUP BY nameFirst, nameLast) AS success
--- ORDER BY success_stealing DESC
--- LIMIT 1;
+SELECT success.nameFirst, success.nameLast, CAST(CAST(Success.stolen_bases AS DECIMAL(5, 2)) / success.total_attempts * 100 AS DECIMAL(5, 2)) AS success_stealing 
+FROM(SELECT nameFirst, nameLast, SUM(sb) AS stolen_bases, SUM(sb + cs) AS total_attempts
+	FROM people
+	INNER JOIN batting AS B
+	USING(playerid)
+	WHERE sb >= 20 
+		AND yearid = 2016
+	GROUP BY nameFirst, nameLast) AS success
+ORDER BY success_stealing DESC
+LIMIT 1;
 
 --5. From 1970 to 2016, what is the largest number of wins for a team that did not win the world series?
 
@@ -108,7 +122,7 @@
 --6. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? 
 --Give their full name and the teams that they were managing when they won the award.
 
--- SELECT TSN_WINNERS_Names.playerid, TSN_WINNERS_Names.nameFirst, TSN_WINNERS_Names.nameLast, TSN_WINNERS_Names.Team2, COUNT( DISTINCT lgid) AS lg_count
+-- SELECT TSN_WINNERS_Names.playerid, TSN_WINNERS_Names.nameFirst, TSN_WINNERS_Names.nameLast,TSN_WINNERS_Names.Team2, COUNT( DISTINCT lgid) AS lg_count
 -- 		FROM (WITH TSN_WINNERS AS (SELECT awardsmanagers.yearid, awardsmanagers.playerid, awardsmanagers.lgid, managers.teamid AS Team, awardid
 -- 				FROM awardsmanagers
 -- 				INNER JOIN managers
@@ -160,5 +174,19 @@
 -- ON people.playerid = hit_count_three_thou.playerid
 -- INNER JOIN halloffame
 -- ON hit_count_three_thou.playerid = halloffame.playerid
+-- WHERE halloffame.inducted = 'Y'
 -- ORDER BY hit_count_three_thou.total_hits DESC
 
+
+
+--9. Find all players who had at least 1,000 hits for two different teams. Report those players' full names.
+WITH hits_by_team_player AS (SELECT teamid, playerid, SUM(h) AS total_hits
+							FROM batting
+							GROUP BY teamid, playerid
+							HAVING SUM(h) >= 1000)
+SELECT hits_by_team_player.playerid, nameFirst, nameLast, COUNT(teamid) AS team_count
+FROM hits_by_team_player
+INNER JOIN people
+USING (playerid)
+GROUP BY hits_by_team_player.playerid, nameFirst, nameLast
+HAVING COUNT(teamid) >= 2
